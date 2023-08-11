f936c8e223b8dac818dd6c756bafbf0d1e824534
attoote <119163844+attoote@users.noreply.github.com>
2023-08-11 18:15:00 +0700

    fix

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(rateLimiterRedisMiddleware);
+//app.use(rateLimiterRedisMiddleware);
 
 const client_id=
     process.env.NODE_ENV === 'development'?
