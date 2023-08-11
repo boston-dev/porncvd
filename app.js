4700612881ad2ea7c1393ff7cf6febb478a30ecc
attoote <119163844+attoote@users.noreply.github.com>
2023-08-11 20:02:17 +0700

    fix

--- app.js
+++ app.js
@@ -53,8 +53,8 @@ const porn5Nav=require('./util/util').default
 const gNav = require('./nav.json');
 const rateLimit = require('express-rate-limit')
 const limiter = rateLimit({
-	windowMs: 5 * 60 * 1000, // 15 minutes
-	max: 50, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	windowMs: 2 * 60 * 1000, // 15 minutes
+	max: 40, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 	// store: ... , // Use an external store for more precise rate limiting
