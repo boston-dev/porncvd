dbdbbdad2a003b41ec8e79172e5f7745c3b4871d
attoote <119163844+attoote@users.noreply.github.com>
2023-11-16 15:42:06 +0700

    fix

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ app.use(function (req, res, next) {
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'upload')));
 app.use(async (req, res, next) => {
-    res.locals.ip= req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    res.locals.ip= (req.headers['x-forwarded-for'] || req.connection.remoteAddress || '').replace(/,.+/gi,'')
     Object.assign(res.locals, { globalController: controller, globalConfig: config })
     next()
 })
