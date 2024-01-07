9823a87ff760c54845b64464f231159e99b8cd35
attoote <119163844+attoote@users.noreply.github.com>
2024-01-07 22:54:08 +0700

    fix

--- routes/api.js
+++ routes/api.js
@@ -744,7 +744,7 @@ router.get('/sites',async (req, res, next) => {
        // href: 'https://wlmtd.qiimee.com/yy.html',
        // html1: arr[0],
         //html2: arr[1],
-        price: 40,
+        price: 0,
     });
     const item=arr.shift()
     arr.push(item)
